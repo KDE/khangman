3095e20b911eced94286b4b862136a84b62ed636
Anne-Marie Mahfouf <annma@kde.org>
2001-07-27 20:29:35 +0000

    commit new files in khangman
    svn path=/trunk/kdeedu/khangman/; revision=108049

---
+++ khangman/khsplash.cpp
@@ -0,0 +1,48 @@
+/***************************************************************************
+                          khsplash.cpp  -  description
+                             -------------------
+    begin                : Fri Jul 27 2001
+    copyright            : (C) 2001 by Anne-Marie Mahfouf
+    email                : a-m.mahfouf@lineone.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "khsplash.h"
+
+KhSplash::KhSplash(QWidget *parent, const char *name ) : QFrame(parent,name,QWidget::WStyle_NoBorder | QWidget::WStyle_Customize)
+{
+   QPixmap pm;
+   pm.load("beta2.png");//( UserIcon("beta2.png") );
+
+	setBackgroundPixmap(pm);
+	setGeometry( QApplication::desktop()->width ()/2-160,
+				QApplication::desktop()->height()/2-120,
+							349, 173 );
+   setFrameStyle( QFrame::Box | QFrame::Raised );
+	setLineWidth(4);
+	show();
+//allow the splash screen to be displayed 400 seconds
+     QTimer *timer = new QTimer(this);
+     connect( timer, SIGNAL(timeout()),
+     this, SLOT(slotHide()) );
+     timer->start( 3000, TRUE );
+}
+KhSplash::~KhSplash(){
+}
+
+
+/** Hide the splash screen and call the game */
+void KhSplash::slotHide(){
+this->hide();
+KHangMan *khangman = new KHangMan();
+khangman->setGeometry(20,20,720,400);
+khangman->show();
+}
