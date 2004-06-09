97169685414b9abb194a22ccdfacca3e7cae9cb8
Anne-Marie Mahfouf <annma@kde.org>
2004-06-09 13:15:44 +0000

    fix
    svn path=/trunk/kdeedu/khangman/; revision=319059

--- khangman/khangman.cpp
+++ khangman/khangman.cpp
@@ -61,14 +61,14 @@ KHangMan::KHangMan()
 	setCentralWidget(m_view);
 	
 	setLanguages();
-	//selectedLanguage is the language saved in Settings otherwise it is default or en if no default
-	// then, setup our actions, must be done after the language search
-	setupActions();
 	// set up the status bar
 	statusBar( )->insertItem("   ",IDS_LEVEL, 0);
 	statusBar( )->insertItem("   ",IDS_LANG, 0);
 	statusBar( )->insertItem("   ",IDS_ACCENTS, 0);
 	statusBar( )->insertItem("   ",IDS_HINT, 0);
+	//selectedLanguage is the language saved in Settings otherwise it is default or en if no default
+	// then, setup our actions, must be done after the language search
+	setupActions();
 	//toolbar for special characters
 	secondToolbar = toolBar("Special Characters");
 	secondToolbar->setBarPos(KToolBar::Bottom);
