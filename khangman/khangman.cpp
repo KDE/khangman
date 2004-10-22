bdde3714284eb93a23bca5c12395baabdfbdc028
Anne-Marie Mahfouf <annma@kde.org>
2004-10-22 20:14:34 +0000

    add russian support
    svn path=/trunk/kdeedu/khangman/; revision=356938

--- khangman/khangman.cpp
+++ khangman/khangman.cpp
@@ -327,7 +327,7 @@ void KHangMan::slotSetFullScreen( bool set )
 
 void KHangMan::loadLangToolBar()
 {
-	if (m_view->language == "en" || m_view->language == "it" || m_view->language == "nl" )
+	if (m_view->language == "en" || m_view->language == "it" || m_view->language == "nl" || m_view->language =="ru" )
 	noCharBool = true;
 	else noCharBool = false;
 	if (secondToolbar->isVisible() && !noCharBool)
