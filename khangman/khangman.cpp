25968095ca9967b7224e2afa52d2bc97775225d7
Benjamin Meyer <ben@meyerhome.net>
2004-10-25 01:22:06 +0000

    use qApp to exit
    svn path=/trunk/kdeedu/khangman/; revision=357454

--- khangman/khangman.cpp
+++ khangman/khangman.cpp
@@ -355,7 +355,7 @@ void KHangMan::loadLangToolBar()
 					"check your installation.").arg(m_view->language);
 		KMessageBox::sorry( this, mString,
 					i18n("Error") );
-		exit(1);
+		qApp->exit(1);
 	}
 	update();
 	//we open the file and store info into the stream...
