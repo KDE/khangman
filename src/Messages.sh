8621cf9b06e0b997a9f1c1856fe586398cdd31c5
Albert Astals Cid <tsdgeos@terra.es>
2010-08-21 16:39:59 +0000

    do not rely on extract-messages.sh doing the work for us
    svn path=/trunk/KDE/kdeedu/khangman/; revision=1166362

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+$EXTRACTRC *.rc *.ui *.kcfg >> rc.cpp
 LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
 if test -n "$LIST"; then
 $XGETTEXT data.i18n $LIST rc.cpp -o $podir/khangman.pot
