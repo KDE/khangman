2c942aef29736110c69380500ea68d5e0ffb2e0e
Rahul Chowdhury <rahulc93@gmail.com>
2014-12-23 04:44:41 +0530

    Move Messages.sh from src/desktop/ to src/ and updated it to translate the qml files

---
+++ src/Messages.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+$EXTRACTATTR --attr=theme,uiname ../../themes/standardthemes.xml >> rc.cpp
+$EXTRACTRC *.rc *.ui *.kcfg >> rc.cpp
+LIST=`find . -name \*.h -o -name \*.cpp -o -name \*.qml`
+if test -n "$LIST"; then
+$XGETTEXT data.i18n $LIST rc.cpp -o $podir/khangman.pot
+fi
