1e7005d61e21133fb406c52b3798459c532c727f
Montel Laurent <montel@kde.org>
2015-03-14 10:10:46 +0100

    Remove include moc

--- src/khangman.cpp
+++ src/khangman.cpp
@@ -559,7 +559,6 @@ void KHangMan::loadLanguageSpecialCharacters()
     }
 }
 
-#include "khangman.moc"
 
 // kate: space-indent on; tab-width 4; indent-width 4; mixed-indent off; replace-tabs on;
 // vim: set et sw=4 ts=4 cino=l1,cs,U1:
