429a47572eca3094da365aa7d5dd659518b7d64e
Friedrich W. H. Kossebau <kossebau@kde.org>
2023-07-14 01:19:19 +0200

    Add explicit moc includes to sources for moc-covered headers
    * speeds up incremental builds as changes to a header will not always
      need the full mocs_compilation.cpp for all the target's headers rebuild,
      while having a moc file sourced into a source file only adds minor
      extra costs, due to small own code and the used headers usually
      already covered by the source file, being for the same class/struct
    * seems to not slow down clean builds, due to empty mocs_compilation.cpp
      resulting in those quickly processed, while the minor extra cost of the
      sourced moc files does not outweigh that in summary.
      Measured times actually improved by some percent points.
      (ideally CMake would just skip empty mocs_compilation.cpp & its object
      file one day)
    * enables compiler to see all methods of a class in same compilation unit
      to do some sanity checks
    * potentially more inlining in general, due to more in the compilation unit
    * allows to keep using more forward declarations in the header, as with the
      moc code being sourced into the cpp file there definitions can be ensured
      and often are already for the needs of the normal class methods

--- src/khangman.cpp
+++ src/khangman.cpp
@@ -582,7 +582,7 @@ void KHangMan::loadLanguageSpecialCharacters()
     openFileStream.close();
 }
 
+#include "moc_khangman.cpp"
 
 // kate: space-indent on; tab-width 4; indent-width 4; mixed-indent off; replace-tabs on;
 // vim: set et sw=4 ts=4 cino=l1,cs,U1:
-
